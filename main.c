d3a32712afc1e64a340fe1ec70dd5c6db6d39cc2
Apoorav Rathore <apoorav.rathore@icloud.com>
2025-08-19 17:35:01 -0500

    created new animation and refactored for readability

--- main.c
+++ main.c
@@ -6,39 +6,28 @@
 #include "TM4C123.h"
 #include "hardware.h"
 #include "display.h"
+#include "animations.c"
 #include <stdint.h>
 
-static const uint8_t img[4][4] = {
-  {0, 1, 1, 0},
-  {1, 0, 0, 1},
-  {1, 0, 0, 1},
-  {0, 1, 1, 0},
-};
-
-static const uint8_t ani[ANI_LENGTH][4][4] = {
-  {
-    {0, 1, 1, 0},
-    {1, 0, 0, 1},
-    {1, 0, 0, 1},
-    {0, 1, 1, 0},
-  },
-  {
-    {1, 0, 0, 1},
-    {0, 1, 1, 0},
-    {0, 1, 1, 0},
-    {1, 0, 0, 1},
-  },  
-};
+static void delay(uint32_t millis) {
+  for (uint32_t i = 0; i < millis; i++) {
+    // wait until COUNTFLAG is set
+    while ((SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk) == 0);
+  }
+}
 
 int main(void) {
   display_initialize();
-  // display a short 'animation'
+  // display a short animation
   while (1) {
-    for (int i = 0; i < ANI_LENGTH; i++) {
-      for (uint32_t j = 0; j < 500u / get_refresh_speed(); j++) {
-	draw(ani[i]);
+    for (int i = 0; i < ANIMATION_SLIDE_LENGTH; i++) {
+      // draw each frame for 25 ms (40 hz)
+      for (uint32_t j = 0; j < 25u / get_refresh_speed(); j++) {
+	draw(animation_slide[i]);
 	while ((SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk) == 0); // display_initialize also initializes SysTick
       }
     }
+    // play the animation every 500 ms
+    delay(500);
   }
 }
