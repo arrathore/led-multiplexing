da4aab5f462dbc09e980e3b9c8341ceb79f66ac2
Apoorav Rathore <apoorav.rathore@icloud.com>
2025-08-20 23:10:46 -0500

    added basic support for half brightness

--- display_4x4.c
+++ display_4x4.c
@@ -21,7 +21,7 @@ uint32_t get_refresh_speed(void) {
 
 void SysTick_Init(void) {
   SysTick->CTRL = 0; // disable SysTick
-  SysTick->LOAD = 80000 - 1; // reload val for 1ms (80k cycles)
+  SysTick->LOAD = 80 - 1; // reload val for 1us (80 cycles)
   SysTick->VAL = 0;
   SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk | SysTick_CTRL_ENABLE_Msk; // enable SysTick with system clock
 }
@@ -34,7 +34,15 @@ void display_initialize() {
 
 // expects SysTick_Init() already called
 static void delay(uint32_t millis) {
-  for (uint32_t i = 0; i < millis; i++) {
+  for (uint32_t i = 0; i < (millis * 1000); i++) {
+    // wait until COUNTFLAG is set
+    while ((SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk) == 0);
+  }
+}
+
+// expects SysTick_Init() already called
+static void udelay(uint32_t micros) {
+  for (uint32_t i = 0; i < micros; i++) {
     // wait until COUNTFLAG is set
     while ((SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk) == 0);
   }
@@ -45,17 +53,26 @@ static void delay(uint32_t millis) {
 void draw(const uint8_t img[4][4]) {
   // for each row
   for (int i = 0; i < 4; i++) {
+    // which pixels are at half-brightness
+    uint8_t halfs[] = {0, 0, 0, 0};
+    
     // draw row i
     for (int j = 0; j < 4; j++) {
       // if the LED should be off, we turn on the pin (Port D)
       if (img[i][j] == 0) HW_PinSet((out_pin_t)j);
-      else HW_PinClear((out_pin_t)j);
+      else {
+	HW_PinClear((out_pin_t)j);
+	halfs[j] = img[i][j];
+      }
     }
 
     // turn on line (display row i)
     HW_PinSet((out_pin_t)(i + 4)); // scanline pins are at offset 4 (Port E)
-    
-    delay(1);
+
+    // if the pixel is half brightness we turn it off early
+    udelay(100);
+    for (int j = 0; j < 4; j++) if (halfs[j] == 1) HW_PinSet((out_pin_t)j);
+    udelay(900);
     HW_PinClear((out_pin_t)(i + 4));
   }
 }
